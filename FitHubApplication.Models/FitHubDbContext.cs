a785c7a0f635c50b0e09f81265f00e3ec4a55407
FilipNajdoski <filipnajdoski@live.com>
2021-06-27 12:33:54 +0200

    added entity framework core to project, initial migration

--- FitHubApplication.Models/FitHubDbContext.cs
+++ FitHubApplication.Models/FitHubDbContext.cs
@@ -4,9 +4,9 @@ namespace FitHubApplication.Models
 {
     public class FitHubDbContext : DbContext
     {
-        public FitHubDbContext( DbContextOptions<FitHubDbContext> options) : base (options)
-        {
-            
-        }
+        public FitHubDbContext( DbContextOptions<FitHubDbContext> options) : base (options){}
+
+        public DbSet<User> Users { get; set; }
+
     }
 }
